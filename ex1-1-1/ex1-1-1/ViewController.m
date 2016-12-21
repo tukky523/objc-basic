40ff24b1baca8bfee7a6fe26384ceb6b2f136a4c
ITユーザー <it-user@kpc-it3143mc.kmt.neec.ac.jp>
2016-12-21 13:18:29 +0900

    12/21.1

--- ex1-1-1/ex1-1-1/ViewController.m
+++ ex1-1-1/ex1-1-1/ViewController.m
@@ -17,6 +17,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //BOOL
+    bool boolLog = true;
+    NSLog(@"%d", boolLog);
+    
+    //NSString
+    NSString *strLog = @"aaa";
+    NSLog(@"%@", strLog);
+    
+    //NSInteger
+    NSInteger intLog = 111;
+    NSLog(@"%ld", intLog);
+    
+    //NSNumber
+    NSNumber *numLog = @1;
+    NSLog(@"%@", numLog);
 }
 
 - (void)didReceiveMemoryWarning {
